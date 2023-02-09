61b886adc246ffc563de2c919d9de2f08023778c
Alfred Bez <alfred.bez@googlemail.com>
2023-02-09 08:46:20 +0100

    Reproduce slow PHPStan analysis of class with nested propel query

---
+++ src/Pyz/Zed/CategoryDataImport/Business/QueryExpander/FooQueryExpander.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Pyz\Zed\CategoryDataImport\Business\QueryExpander;
+
+use Generated\Shared\Transfer\CategoryLocalizedAttributesTransfer;
+use Generated\Shared\Transfer\CategoryTransfer;
+use Generated\Shared\Transfer\NodeTransfer;
+use Generated\Shared\Transfer\ProductAbstractTransfer;
+use Orm\Zed\Category\Persistence\Map\SpyCategoryAttributeTableMap;
+use Orm\Zed\Category\Persistence\Map\SpyCategoryNodeTableMap;
+use Orm\Zed\Category\Persistence\Map\SpyCategoryTableMap;
+use Orm\Zed\Locale\Persistence\Map\SpyLocaleTableMap;
+use Orm\Zed\ProductCategory\Persistence\Map\SpyProductCategoryTableMap;
+use Orm\Zed\ProductCategory\Persistence\SpyProductCategoryQuery;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Propel\Runtime\Formatter\ArrayFormatter;
+
+class FooCategoryQueryExpander
+{
+    /**
+     * @var \Orm\Zed\ProductCategory\Persistence\SpyProductCategoryQuery
+     */
+    protected $productCategoryQuery;
+
+    /**
+     * @param \Orm\Zed\ProductCategory\Persistence\SpyProductCategoryQuery $productCategoryQuery
+     */
+    public function __construct(SpyProductCategoryQuery $productCategoryQuery)
+    {
+        $this->productCategoryQuery = $productCategoryQuery;
+    }
+
+    /**
+     * @param array $dataRows
+     *
+     * @return array
+     */
+    public function expand(array $dataRows): array
+    {
+        $productAbstractIds = array_column($dataRows, ProductAbstractTransfer::ID_PRODUCT_ABSTRACT);
+        $productAbstractIds = array_unique($productAbstractIds);
+
+        $categoriesQuery = $this->productCategoryQuery
+            ->filterByFkProductAbstract_In($productAbstractIds)
+            ->useSpyCategoryQuery()
+            ->joinNode()
+            ->filterByIsActive(true)
+            ->filterByIsSearchable(true)
+            ->useAttributeQuery(null, Criteria::LEFT_JOIN)
+            ->useLocaleQuery()
+            ->filterByIsActive(true)
+            ->endUse()
+            ->endUse()
+            ->endUse();
+
+        $categoriesQuery = $categoriesQuery->withColumn(SpyProductCategoryTableMap::COL_FK_PRODUCT_ABSTRACT, ProductAbstractTransfer::ID_PRODUCT_ABSTRACT);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryTableMap::COL_ID_CATEGORY, CategoryTransfer::ID_CATEGORY);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryTableMap::COL_IS_ACTIVE, CategoryTransfer::IS_ACTIVE);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryTableMap::COL_IS_SEARCHABLE, CategoryTransfer::IS_SEARCHABLE);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryTableMap::COL_CATEGORY_KEY, CategoryTransfer::CATEGORY_KEY);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyLocaleTableMap::COL_LOCALE_NAME, CategoryLocalizedAttributesTransfer::LOCALE);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryAttributeTableMap::COL_NAME, CategoryLocalizedAttributesTransfer::NAME);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryAttributeTableMap::COL_META_TITLE, CategoryLocalizedAttributesTransfer::META_TITLE);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyProductCategoryTableMap::COL_PRODUCT_ORDER, CategoryTransfer::ID_CMS_BLOCKS);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_FK_CATEGORY, NodeTransfer::FK_CATEGORY);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_ID_CATEGORY_NODE, NodeTransfer::ID_CATEGORY_NODE);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_IS_MAIN, NodeTransfer::IS_MAIN);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_IS_ROOT, NodeTransfer::IS_ROOT);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_NODE_ORDER, NodeTransfer::NODE_ORDER);
+        $categoriesQuery = $categoriesQuery->withColumn(SpyCategoryNodeTableMap::COL_FK_PARENT_CATEGORY_NODE, NodeTransfer::FK_PARENT_CATEGORY_NODE);
+        $categoriesQuery = $categoriesQuery->setFormatter(new ArrayFormatter());
+
+        $categories = $categoriesQuery
+            ->find()
+            ->getArrayCopy();
+
+        $dataRowsWithCategories = [];
+
+        foreach ($dataRows as $offerDataUniqueRow) {
+            $offerDataUniqueRow['KEY_CATEGORIES'] = [];
+            foreach ($categories as $category) {
+                if ((int)$category[ProductAbstractTransfer::ID_PRODUCT_ABSTRACT] === (int)$offerDataUniqueRow[ProductAbstractTransfer::ID_PRODUCT_ABSTRACT]) {
+                    $offerDataUniqueRow['KEY_CATEGORIES'][$category[CategoryTransfer::ID_CATEGORY]][] = $category;
+                }
+            }
+            $dataRowsWithCategories[] = $offerDataUniqueRow;
+        }
+
+        return $dataRowsWithCategories;
+    }
+}
